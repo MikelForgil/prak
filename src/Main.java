0078a50ab86d5042cfe01ef872a3842f2305997e
MikelForgil <126507404+MikelForgil@users.noreply.github.com>
2023-02-28 23:05:37 +0200

    First Commit
    First Commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main{
+    public static void main(String[] arg){
+        int a;
+        a = 34 + 43;
+        System.out.println("Результат : "+a);
+        int b;
+        b = a / 43;
+        System.out.println("Результат : "+b);
+        double c = (b+a) / (a * 10.7);
+        System.out.println("Результат : "+c);
+    }
+}
+
+    //Завдання:2
+public class Main{
+    public static void main(String[] arg){
+        int a = 23;
+        int b = 32;
+
+        int c = a += b;
+        System.out.println(c);
+
+    }
+}
+
+    //Завдання.3
+public class Main{
+    public static void main(String[] arg){
+        float num = 2.5f;
+        System.out.println(Math.floor(num));
+    }
+}
+
